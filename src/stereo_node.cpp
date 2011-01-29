2a498cf040625a4032cd3beed89af60ce06bf033
Michael Koval <koval.michael@gmail.com>
2011-01-29 01:46:50 -0500

    Basic ROS node with no synchronization support

--- src/stereo_node.cpp
+++ src/stereo_node.cpp
@@ -1,11 +1,34 @@
 #include <exception>
 #include <iostream>
 
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+
 #include "CameraFrame.hpp"
 #include "Webcam.hpp"
 
+using sensor_msgs::Image;
+
 static size_t   const BUFFER_NUM = 2;
 static uint32_t const TIMEOUT_MS = 100;
+
+Image FrameToImageMsg(CameraFrame const &src)
+{
+	Image msg;
+	msg.width    = src.GetWidth();
+	msg.height   = src.GetHeight();
+	msg.encoding = sensor_msgs::image_encodings::BGR8;
+	msg.step     = src.GetWidth() * 3;
+
+	// Copy data into the message.
+	msg.data.resize(msg.step * msg.height);
+	uint8_t const *data = src.GetDataBGR();
+	for (size_t i = 0; i < msg.step * msg.height; ++i) {
+		msg.data[i] = data[i];
+	}
+	return msg;
+}
 
 int main(int argc, char **argv)
 {
@@ -15,6 +38,11 @@ int main(int argc, char **argv)
 		          << std::endl;
 		return 1;
 	}
+
+	ros::init(argc, argv, "stereo_node");
+	ros::NodeHandle nh;
+	ros::Publisher pub_left  = nh.advertise<Image>("stereo/left", 10);
+	ros::Publisher pub_right = nh.advertise<Image>("stereo/right", 10);
 
 	CameraFrame frame_left;
 	CameraFrame frame_right;
@@ -26,14 +54,26 @@ int main(int argc, char **argv)
 		cam_left.SetStreaming(true);
 		cam_right.SetStreaming(true);
 
-		for (;;) {
+		// TODO: Use a multi-threaded program instead of a busy loop.
+		while (ros::ok()) {
 			cam_left.WaitForFrame(TIMEOUT_MS);
 			cam_right.WaitForFrame(TIMEOUT_MS);
 
 			cam_left.GetFrame(frame_left);
 			cam_right.GetFrame(frame_right);
 
-			// TODO: Publish the image messages.
+			// Wrap the frames in a sensor_msgs::Image message.
+			Image msg_left = FrameToImageMsg(frame_left);
+			msg_left.header.stamp     = ros::Time(0, 0); // XXX: Convert timestamp.
+			msg_left.header.frame_id  = "stereo/left"; // TODO: naming convention
+
+			Image msg_right = FrameToImageMsg(frame_right);
+			msg_right.header.stamp    = ros::Time(0, 0); // XXX: Convert timestamp.
+			msg_right.header.frame_id = "stereo/right"; // TODO: naming convention
+
+			pub_left.publish(msg_left);
+			pub_right.publish(msg_right);
+			ros::spinOnce();
 		}
 	} catch (std::exception const &err) {
 		std::cerr << "err: " << err.what() << std::endl;

497795c12858be91d73692014e20d628b9a4dfeb
Michael Koval <koval.michael@gmail.com>
2011-02-07 16:11:23 -0500

    Moved default parameters into static variables

--- src/stereo_node.cpp
+++ src/stereo_node.cpp
@@ -1,8 +1,5 @@
 #include <exception>
-#include <iostream>
 #include <string>
-
-#include <boost/thread.hpp>
 
 #include <ros/ros.h>
 #include <camera_info_manager/camera_info_manager.h>
@@ -20,6 +17,16 @@ using sensor_msgs::CameraInfo;
 using sensor_msgs::Image;
 using image_transport::CameraPublisher;
 using image_transport::ImageTransport;
+
+static std::string const def_dev_left   = "/dev/video_left";
+static std::string const def_dev_right  = "/dev/video_right";
+static std::string const def_file_left  = "calibration_left.yaml";
+static std::string const def_file_right = "calibration_right.yaml";
+static double const def_threshold = 0.005;
+static int const def_width   = 640;
+static int const def_height  = 480;
+static int const def_fps     = 30;
+static int const def_buffers = 10;
 
 static Image FrameToImageMsg(CameraFrame const &src)
 {
@@ -41,44 +48,46 @@ static Image FrameToImageMsg(CameraFrame const &src)
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "stereo_node");
+
 	ros::NodeHandle nh;
+	ros::NodeHandle nh_priv("~");
+	ros::NodeHandle nh_left("left");
+	ros::NodeHandle nh_right("right");
 
 	ImageTransport it(nh);
 	CameraPublisher pub_left  = it.advertiseCamera("left/image", 1);
 	CameraPublisher pub_right = it.advertiseCamera("right/image", 1);
 
-	// Paths to each of the camera device files.
-	std::string dev_left, dev_right;
-	std::string info_left, info_right;
-	int res_width, res_height;
-	int nbuf, fps;
+	// Static parameters that are fixed during runtime.
+	std::string dev_left,  dev_right;
+	std::string file_left, file_right;
+	int width, height, fps, buffers;
+	double threshold;
 
-	ros::NodeHandle nh_priv("~");
-	nh_priv.param("device_left",  dev_left,   std::string("/dev/video_left"));
-	nh_priv.param("device_right", dev_right,  std::string("/dev/video_right"));
-	nh_priv.param("info_left",    info_left,  std::string("file:///tmp/calibration_left.yaml"));
-	nh_priv.param("info_right",   info_right, std::string("file:///tmp/calibration_right.yaml"));
-	nh_priv.param("width",        res_width,  640);
-	nh_priv.param("height",       res_height, 480);
-	nh_priv.param("fps",          fps,        30);
-	nh_priv.param("buffers",      nbuf,       10);
-	// TODO: Enable use of the error_ratio parameter.
-	// TODO: Allow configuration of other camera parameters.
+	nh_priv.param("device_left",  dev_left,   def_dev_left);
+	nh_priv.param("device_right", dev_right,  def_dev_right);
+	nh_priv.param("info_left",    file_left,  def_file_left);
+	nh_priv.param("info_right",   file_right, def_file_right);
+	nh_priv.param("width",        width,      def_width);
+	nh_priv.param("height",       height,     def_height);
+	nh_priv.param("fps",          fps,        def_fps);
+	nh_priv.param("buffers",      buffers,    def_buffers);
+	nh_priv.param("threshold",    threshold,  def_threshold);
 
-	if (nbuf < 2) {
+	if (buffers < 2) {
 		ROS_ERROR("need two buffers or more buffers for synchronization");
 		return 1;
 	}
 
 	// Negotiate resolution and framerate.
-	Webcam cam_left(dev_left, nbuf);
-	Webcam cam_right(dev_right, nbuf);
+	Webcam cam_left(dev_left, buffers);
+	Webcam cam_right(dev_right, buffers);
 
 	try {
-		cam_left.SetResolution(res_width, res_height);
-		cam_right.SetResolution(res_width, res_height);
+		cam_left.SetResolution(width, height);
+		cam_right.SetResolution(width, height);
 	} catch (std::runtime_error const &e) {
-		std::cerr << "err: unable to negotiate resolution" << std::endl;
+		ROS_ERROR("resolution is not supported");
 		return 1;
 	}
 
@@ -86,7 +95,7 @@ int main(int argc, char **argv)
 		cam_left.SetFPS(fps);
 		cam_right.SetFPS(fps);
 	} catch (std::invalid_argument const &e) {
-		std::cerr << "err: framerate is unsupported at this resolution" << std::endl;
+		ROS_ERROR("framerate is not supported at this resolution");
 		return 1;
 	}
 
@@ -100,11 +109,9 @@ int main(int argc, char **argv)
 	TimeConverter time_conv(init_sys, init_ros);
 	CameraFrameComparator comp(0.0005);
 
-	CameraInfoManager caminfo_left(ros::NodeHandle("left"), "left", info_left);
-	CameraInfoManager caminfo_right(ros::NodeHandle("right"), "right", info_right);
-
-	CameraFrame frame_left;
-	CameraFrame frame_right;
+	CameraFrame frame_left, frame_right;
+	CameraInfoManager caminfo_left(nh_left, "left", file_left);
+	CameraInfoManager caminfo_right(nh_right, "right", file_right);
 
 	cam_left.SetStreaming(true);
 	cam_right.SetStreaming(true);
@@ -125,35 +132,32 @@ int main(int argc, char **argv)
 			// TODO: Choose the (slightly) higher of the two timestamps.
 			ros::Time time = time_conv.SysToROS(frame_left.GetTimestamp());
 
-			// Left Camera's Image and CameraInfo
+			// Pair each image with its calibration data.
+			CameraInfo info_left  = caminfo_left.getCameraInfo();
+			CameraInfo info_right = caminfo_right.getCameraInfo();
+
 			Image msg_left = FrameToImageMsg(frame_left);
 			msg_left.header.stamp      = time;
-			msg_left.header.frame_id   = "stereo/stereo_link";
+			msg_left.header.frame_id   = "stereo_link";
+			info_left.header.stamp     = time;
+			info_left.header.frame_id  = "stereo_link";
+			pub_left.publish(msg_left, info_left);
 
-			CameraInfo info_l = caminfo_left.getCameraInfo();
-			info_l.header.stamp    = time;
-			info_l.header.frame_id = "stereo/stereo_link";
-			pub_left.publish(msg_left, info_l);
-
-			// Right Camera's Image and CameraInfo
 			Image msg_right = FrameToImageMsg(frame_right);
 			msg_right.header.stamp     = time;
-			msg_right.header.frame_id  = "stereo/stereo_link";
-
-			CameraInfo info_r = caminfo_right.getCameraInfo();
-			info_r.header.stamp    = time;
-			info_r.header.frame_id = "stereo/stereo_link";
-			pub_right.publish(msg_right, info_r);
+			msg_right.header.frame_id  = "stereo_link";
+			info_right.header.stamp    = time;
+			info_right.header.frame_id = "stereo_link";
+			pub_right.publish(msg_right, info_right);
 
 			adv_left  = true;
 			adv_right = true;
-			// std::cerr << "ok: synchronized" << std::endl;
 		} else if (order < 0) {
 			adv_left = true;
-			// std::cerr << "war: dropping left frame" << std::endl;
+			ROS_WARN("desynchronized; dropping left frame");
 		} else if (order > 0) {
 			adv_right = true;
-			// std::cerr << "war: dropping right frame" << std::endl;
+			ROS_WARN("desynchronized; dropping right frame");
 		}
 		ros::spinOnce();
 	}
